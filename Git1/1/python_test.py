0ee7d3fcc938f5c98a8a3be6f17dfd82819fa9e3
Vladislav98 <vlados.vidos@mail.ru>
2022-05-29 12:02:56 +0300

    задание 1

--- Git1/1/python_test.py
+++ Git1/1/python_test.py
@@ -1,4 +1,10 @@
-i = [1,321,543,12,'ser',12312,'er']
-i.sort()
-print(i)
+a  = [1,8,32,657867,43124,345,12]
 
+N = len(a)
+
+for i in range(0, N-1):
+    for j in range(0, N-1-i):
+        if a[j]> a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+
+print(a)
